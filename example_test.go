40e17afda02f7672b90d5ac3a6a43f39b9d20a2f
Marc-Antoine Ruel <maruel@gmail.com>
2025-05-15 09:04:02 -0400

    Move examples back to root

--- example_test.go
+++ example_test.go
@@ -5,8 +5,10 @@
 package genai_test
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -193,4 +195,222 @@ func ExampleChatProvider() {
 			fmt.Fprintf(os.Stderr, "- %s: %v\n", name, response)
 		}
 	}
+}
+
+func ExampleChatProvider_chat_vision_and_JSON() {
+	// Using small model for testing.
+	// See https://ai.google.dev/gemini-api/docs/models/gemini?hl=en
+	c, err := gemini.New("", "gemini-2.0-flash-lite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	bananaJpg, err := os.ReadFile("banana.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	msgs := genai.Messages{
+		{
+			Role: genai.User,
+			Contents: []genai.Content{
+				{Text: "Is it a banana? Reply as JSON."},
+				{Filename: "banana.jpg", Document: bytes.NewReader(bananaJpg)},
+			},
+		},
+	}
+	var got struct {
+		Banana bool `json:"banana"`
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+		DecodeAs:    &got,
+	}
+	resp, err := c.Chat(context.Background(), msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Raw response: %#v", resp)
+	if err := resp.Decode(&got); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Banana: %v\n", got.Banana)
+	// This would Output: Banana: true
+}
+
+func ExampleChatProvider_chat_PDF() {
+	// Using small model for testing.
+	// See https://ai.google.dev/gemini-api/docs/models/gemini?hl=en
+	c, err := gemini.New("", "gemini-2.0-flash-lite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Open("hidden_word.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	msgs := genai.Messages{
+		{
+			Role: genai.User,
+			Contents: []genai.Content{
+				{Text: "What is the word? Reply with only the word."},
+				{Document: f},
+			},
+		},
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+	}
+	resp, err := c.Chat(context.Background(), msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Raw response: %#v", resp)
+	fmt.Printf("Hidden word in PDF: %v\n", strings.ToLower(resp.AsText()))
+	// This would Output: Hidden word in PDF: orange
+}
+
+func ExampleChatProvider_chat_audio() {
+	c, err := gemini.New("", "gemini-2.0-flash-lite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Open("testdata/mystery_word.opus")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	msgs := genai.Messages{
+		{
+			Role: genai.User,
+			Contents: []genai.Content{
+				{Text: "What is the word said? Reply with only the word."},
+				{Document: f},
+			},
+		},
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+	}
+	resp, err := c.Chat(context.Background(), msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Raw response: %#v", resp)
+	fmt.Printf("Heard: %v\n", strings.TrimRight(strings.ToLower(resp.AsText()), "."))
+	// This would Output: Heard: orange
+}
+
+func ExampleChatProvider_chat_tool_use() {
+	c, err := gemini.New("", "gemini-2.0-flash-lite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Open("testdata/animation.mp4")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	msgs := genai.Messages{
+		{
+			Role: genai.User,
+			Contents: []genai.Content{
+				{Text: "What is the word? Call the tool hidden_word to tell me what word you saw."},
+				{Document: f},
+			},
+		},
+	}
+	var got struct {
+		Word string `json:"word" jsonschema:"enum=Orange,enum=Banana,enum=Apple"`
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+		Tools: []genai.ToolDef{
+			{
+				Name:        "hidden_word",
+				Description: "A tool to state what word was seen in the video.",
+				InputsAs:    &got,
+			},
+		},
+	}
+	resp, err := c.Chat(context.Background(), msgs, &opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Raw response: %#v", resp)
+	// Warning: there's a bug where it returns two identical tool calls. To verify.
+	if len(resp.ToolCalls) == 0 || resp.ToolCalls[0].Name != "hidden_word" {
+		log.Fatal("Unexpected response")
+	}
+	if err := resp.ToolCalls[0].Decode(&got); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Saw: %v\n", strings.ToLower(got.Word))
+	// This would Output: Saw: banana
+}
+
+func ExampleChatProvider_chatStream() {
+	c, err := gemini.New("", "gemini-2.0-flash-lite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	msgs := genai.Messages{
+		genai.NewTextMessage(genai.User, "Say hello. Use only one word."),
+	}
+	opts := genai.ChatOptions{
+		Seed:        1,
+		Temperature: 0.01,
+		MaxTokens:   50,
+	}
+	chunks := make(chan genai.MessageFragment)
+	end := make(chan genai.Message, 10)
+	go func() {
+		var pendingMsgs genai.Messages
+		defer func() {
+			for _, m := range pendingMsgs {
+				end <- m
+			}
+			close(end)
+		}()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case pkt, ok := <-chunks:
+				if !ok {
+					return
+				}
+				var err2 error
+				if pendingMsgs, err2 = pkt.Accumulate(pendingMsgs); err2 != nil {
+					end <- genai.NewTextMessage(genai.Assistant, fmt.Sprintf("Error: %v", err2))
+					return
+				}
+			}
+		}
+	}()
+	_, err = c.ChatStream(ctx, msgs, &opts, chunks)
+	close(chunks)
+	var responses genai.Messages
+	for m := range end {
+		responses = append(responses, m)
+	}
+	log.Printf("Raw responses: %#v", responses)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(responses) != 1 {
+		log.Fatal("Unexpected responses")
+	}
+	resp := responses[0]
+	// Normalize some of the variance. Obviously many models will still fail this test.
+	fmt.Printf("Response: %s\n", strings.TrimRight(strings.TrimSpace(strings.ToLower(resp.AsText())), ".!"))
+	// This would Output: Response: hello
 }

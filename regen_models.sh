38950915d52d14d49540e7c0347850f4e345c531
Marc-Antoine Ruel <maruel@gmail.com>
2025-05-22 09:35:53 -0400

    regen_models.sh: make it no redirect is needed

--- regen_models.sh
+++ regen_models.sh
@@ -8,13 +8,15 @@ set -eu
 go install ./cmd/list-models
 PROVIDERS=(anthropic cerebras cloudflare cohere deepseek gemini groq huggingface mistral openai togetherai)
 
-echo "# List of models available on each provider"
-echo ""
-echo "Snapshot of the models available on each provider as of $(date +%Y-%m-%d)"
+echo "# List of models available on each provider" > MODELS.new.md
+echo "" >> MODELS.new.md
+echo "Snapshot of the models available on each provider as of $(date +%Y-%m-%d)" >> MODELS.new.md
 
 for i in "${PROVIDERS[@]}"; do
-	echo ""
-    echo "## $i"
-	echo ""
-    list-models -provider $i | sed 's/^/- /'
+	echo "" >> MODELS.new.md
+    echo "## $i" >> MODELS.new.md
+	echo "" >> MODELS.new.md
+    list-models -provider $i | sed 's/^/- /' >> MODELS.new.md
 done
+
+mv MODELS.new.md MODELS.md
